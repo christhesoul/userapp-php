64e1c3475aeed1c17e52dbe87226c2a78eeb99e2
christhesoul <chris@first65.com>
2013-10-27 14:47:06 +0000

    changing key reference

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // https://api.userapp.io/{{version}}/{{service}}.{{method}}
 
 // This bit is fucking essential
-$app_id = $_ENV["userapp_key"];
+$app_id = $_ENV["USERAPP_API_KEY"];
 // Basic configuration
 $api_url = 'https://api.userapp.io/';
 $version = 'v1/';
